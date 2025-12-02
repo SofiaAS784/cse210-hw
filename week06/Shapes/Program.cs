0f2e60877402877095ff58cb4d5b5c3a88009dc1
SofiaAS784 <ssuarez2@byupathway.edu>
2025-12-02 19:03:39 -0300

    w06 shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 12.5);
+        Rectangle rectangle = new Rectangle("blue", 14, 22);
+        Circle circle = new Circle("yellow", 7);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine("");
+        }
     }
 }
